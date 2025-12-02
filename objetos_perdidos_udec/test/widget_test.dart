4a1c8bbd09f421c3c5ded7d27b370a882c40eed8
FurioStyle <benjamin.bglh@gmail.com>
2025-12-01 23:43:25 -0300

    fix: arreglado error en ruta

--- objetos_perdidos_udec/test/widget_test.dart
+++ objetos_perdidos_udec/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:objetos_perdidos_udec/Visual/main.dart';
+import 'package:objetos_perdidos_udec/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
